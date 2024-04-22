aa5a27b7e4180cc1173255d943451e2016aedd86
Paul Freeman <paul.freeman.cs@gmail.com>
2024-04-22 21:19:04 +1200

    improve ground pathing

--- GroundClear.cs
+++ GroundClear.cs
@@ -42,16 +42,9 @@ namespace GroundClear
             {
                 return null;
             }
-            if (plant.def.plant.IsTree)
+            if (plant.def.plant.IsTree && !plant.def.plant.isStump && AreTreesDesired(pawn))
             {
-                if (pawn.Ideo.HasMeme(MemeDefOf.TreeConnection))
-                {
-                    return null;
-                }
-                if (pawn.Ideo.HasPrecept(DefDatabase<PreceptDef>.GetNamed("Trees_Desired")))
-                {
-                    return null;
-                }
+                return null;
             }
             LocalTargetInfo target = plant;
             bool ignoreOtherReservations = forced;
@@ -60,6 +53,11 @@ namespace GroundClear
                 return null;
             }
             return plant;
+        }
+
+        private static bool AreTreesDesired(Pawn pawn)
+        {
+            return pawn.Ideo.HasMeme(MemeDefOf.TreeConnection) || pawn.Ideo.HasPrecept(DefDatabase<PreceptDef>.GetNamed("Trees_Desired"));
         }
 
         public override Job JobOnCell(Pawn pawn, IntVec3 c, bool forced = false)
@@ -83,7 +81,11 @@ namespace GroundClear
             Pawn pawn = __instance;
             try
             {
-                if (pawn == null || pawn.Position == null || pawn.Map == null)
+                if (pawn?.Position == null)
+                {
+                    return;
+                }
+                if (pawn?.Map == null)
                 {
                     return;
                 }
@@ -91,14 +93,10 @@ namespace GroundClear
                 {
                     return;
                 }
-                IntVec3 oldPos;
-                if (position.TryGetValue(pawn, out oldPos))
+                bool pawnHasNotMovedSignificantly = position.TryGetValue(pawn, out IntVec3 oldPos) && oldPos == pawn.Position;
+                if (pawnHasNotMovedSignificantly)
                 {
-                    if (oldPos == pawn.Position)
-                    {
-                        // pawn is still on same tile
-                        return;
-                    }
+                    return;
                 }
                 position[pawn] = pawn.Position;
                 Plant plant = GridsUtility.GetPlant(pawn.Position, pawn.Map);
@@ -106,34 +104,10 @@ namespace GroundClear
                 {
                     return;
                 }
-                // colonists should not damage their crops unless drafted
-                if (plant.IsCrop && pawn.IsColonistPlayerControlled && !pawn.Drafted)
+                float damage = CalculatePlantDamage(pawn, plant);
+                if (damage <= 0.0f)
                 {
                     return;
-                }
-                // harvestable trees are considered too big to damage (unless
-                // body size is 2.0 or larger)
-                if (plant.def.plant.IsTree && plant.HarvestableNow && pawn.BodySize < 2.0f)
-                {
-                    return;
-                }
-                if (plant.Growth <= 0.0f)
-                {
-                    return;
-                }
-                var damage = pawn.BodySize * pawn.GetStatValue(StatDefOf.MeleeDPS) / plant.Growth;
-                if (pawn.AnimalOrWildMan())
-                {
-                    // calm animals damage plants much less
-                    if (!pawn.InAggroMentalState)
-                    {
-                        damage *= 0.1f;
-                    }
-                }
-                if (pawn.RaceProps.FleshType == FleshTypeDefOf.Mechanoid)
-                {
-                    // mechanoids damages plants more
-                    damage *= 1.5f;
                 }
                 plant.TakeDamage(new DamageInfo(DamageDefOf.Deterioration, damage, 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown, null));
                 // Log.Message($"{pawn.def.defName} ({pawn.Name}) caused {damage} damage to {plant.def.defName} - growth={plant.Growth}");
@@ -143,6 +117,49 @@ namespace GroundClear
                 Log.ErrorOnce($"could not clear path: {e}", 510515686);
                 return;
             }
+        }
+
+        private static float CalculatePlantDamage(Pawn pawn, Plant plant)
+        {
+            if (pawn == null || plant == null)
+            {
+                return 0;
+            }
+            if (plant.Growth <= 0.0f)
+            {
+                return 0;
+            }
+            if (plant.IsCrop && !pawn.HostileTo(Faction.OfPlayer))
+            {
+                // non-hostile pawns should not damage crops
+                return 0;
+            }
+            if (pawn.Ideo != null)
+            {
+                if (pawn.Ideo.HasMeme(MemeDefOf.TreeConnection) || pawn.Ideo.HasPrecept(DefDatabase<PreceptDef>.GetNamed("Trees_Desired")))
+                {
+                    // pawns with tree connection or trees desired should not damage trees
+                    return 0;
+                }
+            }
+            if (plant.def.plant.IsTree && plant.HarvestableNow && pawn.BodySize < 2.0f)
+            {
+                // harvestable trees are considered too big to damage (unless
+                // body size is 2.0 or larger)
+                return 0;
+            }
+            float baseDamage = pawn.BodySize * pawn.GetStatValue(StatDefOf.MeleeDPS) / plant.Growth;
+            if (pawn.AnimalOrWildMan() && !pawn.InAggroMentalState)
+            {
+                // calm animals damage plants much less
+                baseDamage *= 0.1f;
+            }
+            if (pawn.RaceProps.FleshType == FleshTypeDefOf.Mechanoid)
+            {
+                // mechanoids damages plants more
+                baseDamage *= 1.5f;
+            }
+            return baseDamage;
         }
     }
 
